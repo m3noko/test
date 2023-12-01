9165c035116517eb2ee12652f5374990712ba11d
m3noko <ma3noko@gmail.com>
2023-12-01 12:02:56 +0300

    f project

---
+++ src/components/FourOhFour.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const FourOhFour = () => {
+    return (
+        <div>
+            <h1>404: Page Not Found</h1>
+            <p>Sorry, the page you're looking for doesn't exist or has been moved.</p>
+            {/* Add additional HTML or React components as needed */}
+        </div>
+    );
+};
+
+export default FourOhFour;  
